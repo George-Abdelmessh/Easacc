af9c1cf6c18f19737ac4195e3cb5ed612b7c6b0f
George Abd-Elmesseh <76843136+George-Abdelmessh@users.noreply.github.com>
2025-11-23 00:37:53 +0200

    handle setings screen route

--- lib/core/router/route_pages.dart
+++ lib/core/router/route_pages.dart
@@ -1,5 +1,6 @@
 import 'package:easacc/features/auth/presentation/view/screens/login_screen.dart';
 import 'package:easacc/features/home/presentation/view/screens/home_screen.dart';
+import 'package:easacc/features/settings/presentation/view/screens/settings_screen.dart';
 import 'package:go_router/go_router.dart';
 
 import 'app_routes.dart';
@@ -17,9 +18,9 @@ GoRoute(
     name: 'home',
     builder: (context, state) => const HomeScreen(),
   ),
-  // GoRoute(
-  //   path: AppRoutes.webview,
-  //   name: 'webview',
-  //   builder: (context, state) => const WebViewScreen(),
-  // ),
+  GoRoute(
+    path: AppRoutes.settings,
+    name: 'settings',
+    builder: (context, state) => const SettingsScreen(),
+  ),
 ];
